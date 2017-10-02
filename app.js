4a706ec112ecae79e8d2d7cee41ca78ab2a0b0ef
vinothraja <j.vinoth@virtualgodown.com>
2017-10-03 02:43:04 +0530

    port

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var restify = require('restify');
 
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(443, function () {
+server.listen(process.env.PORT, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
 
